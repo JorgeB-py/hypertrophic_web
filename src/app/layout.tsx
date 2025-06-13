34f52b8282b7238188fb9e983c69f4fadcbc70d3
JorgeB <j.bustamantep@uniandes.edu.co>
2025-06-13 14:58:12 -0500

    Revert "fix: layout"
    This reverts commit b12dc75d9bd1b1b5c6d3ae570b73b0b927bce3a3.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="relative">
-        <div className="fixed inset-0 -z-10 bg-[url('/fondo.png')] bg-cover bg-center">
-          <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
-            <Header />
-            <main className="w-full max-w-7xl mx-auto px-2 sm:px-4 md:px-8 min-h-0 text-white overflow-x-hidden">{children}</main>
-            <Footer />
-          </div>
+      <body className="bg-[url('/fondo.png')] bg-cover bg-center">
+        <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
+          <Header />
+          <main className="w-full max-w-7xl mx-auto px-2 sm:px-4 md:px-8 min-h-0 text-white overflow-x-hidden">{children}</main>
+          <Footer />
         </div>
       </body>
     </html>
